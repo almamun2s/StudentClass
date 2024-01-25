ce9eeb9a45eb02a978940e1c32cd0f15d8e54f6c
Abdullah Almamun <mamun2social@gmail.com>
2024-01-25 22:51:19 +0600

    Completed

--- class/Students.php
+++ class/Students.php
@@ -1,5 +1,45 @@
 <?php 
 
 class Students{
-    
+    protected string $name;
+    public array  $score;
+    public string $grade;
+
+    private int $totalScore = 0;
+    private int $totalSub;
+    private int $avarage;
+
+
+    public function __construct(string $name, array $score)
+    {
+        $this->name = $name;
+        $this->score = $score;
+        $this->totalSub = count($score);
+    }
+
+
+    public function calculateGrade(){
+
+        foreach ($this->score as $value ) {
+            $this->totalScore = $this->totalScore + $value;
+        }
+
+        $this->avarage = floor($this->totalScore / $this->totalSub);
+
+        if ($this->avarage > 90 ) {
+            $this->grade = 'A';
+        }elseif ($this->avarage > 80 ) {
+            $this->grade = 'B';
+        }elseif ($this->avarage > 70 ) {
+            $this->grade = 'C';
+        }elseif ($this->avarage > 60 ) {
+            $this->grade = 'D';
+        }elseif ($this->avarage > 0 ) {
+            $this->grade = 'F';
+        }else {
+            $this->grade = 'Some Problem';
+        }
+
+        return $this->name.' has gained '.$this->grade;
+    }
 }
--- index.php
+++ index.php
@@ -8,6 +8,23 @@
 <body>
     <?php 
         require_once 'class/Students.php';
+
+        $student1 = new Students('Abdullah', [90, 85, 60]);
+        echo $student1->calculateGrade();
+        echo '<br>';
+        
+        $student2 = new Students('Almamun', [99, 80, 77, 88, 95]);
+        echo $student2->calculateGrade();
+        echo '<br>';
+        
+        $student2 = new Students('Prince', [80, 85, 70, 99]);
+        echo $student2->calculateGrade();
+        echo '<br>';
+        
+        $student2 = new Students('Mahmud', [53, 80, 99, 89,]);
+        echo $student2->calculateGrade();
+        echo '<br>';
+
     ?>
 </body>
 </html>
